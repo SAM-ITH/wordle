735d24dae396be0ec3831060bd83255dc2089e2c
Samith <samithwijesinha@gmail.com>
2022-03-10 21:26:16 +0530

    first commit with added launch screen

---
+++ wordle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wordle
+//
+//  Created by Samith Wijesinghe on 2022-03-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Samith!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wordle/wordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordleApp.swift
+//  wordle
+//
+//  Created by Samith Wijesinghe on 2022-03-10.
+//
+
+import SwiftUI
+
+@main
+struct wordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
